e1ecccc24966634025dc752cb7a760e3ac85b8e9
Prince Onwuvuche <ponwuvuche@wlapcs.org>
2018-01-23 18:30:09 +0000

    first commits

--- sketch.js
+++ sketch.js
@@ -4,13 +4,26 @@ var lpkidsblue;
 var lpkidsgray;
 
 function preload(){
+    lpkidsgray = loadImage("images/lpkids-gray.png")
+    lpkidsblue = loadImage("images/lpkids-blue.png")
+    lpkidsgreen = loadImage("images/lpkids-green.png")
+    lpkidsred = loadImage("images/lpkids-red.png")
     
 }
 
 function setup(){
     createCanvas(500,374);
+    
 }
 
 function draw(){
+    if (keyIsPressed) {
+        if(key=="r"){
+            image(lpkidsred,0,0);
+        }
+    } 
+    else {
+        image(lpkidsgray,0,0);
+    }
     
 }
