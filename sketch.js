0ebf6c96f79b0e0872f0e5b874ce04e73a4bfd6c
Prince Onwuvuche <ponwuvuche@wlapcs.org>
2018-01-24 03:27:02 +0000

    added tv

--- sketch.js
+++ sketch.js
@@ -8,37 +8,39 @@ function preload(){
     lpkidsblue = loadImage("images/lpkids-blue.png")
     lpkidsgreen = loadImage("images/lpkids-green.png")
     lpkidsred = loadImage("images/lpkids-red.png")
+    television = loadImage("images/oldtv.png")
     
 }
 
 function setup(){
-    createCanvas(500,374);
+    createCanvas(749,524);
     
 }
 
 function draw(){
     if (keyIsPressed) {
         if(key=="r"){
-            image(lpkidsred,0,0);
+            image(lpkidsred,50,50);
         }
         if (key=="g"){
-            image(lpkidsgreen,0,0);
+            image(lpkidsgreen,50,50);
         }
         if (key=="b"){
-            image(lpkidsblue,0,0);
+            image(lpkidsblue,50,50);
         }
         if (keyCode=="32"){
             fill(0);
             rect(0,0,500,374);
             blendMode(LIGHTEST);
-            image(lpkidsgreen,0,0);
-            image(lpkidsred,0,0);
-            image(lpkidsblue,0,0);
+            image(lpkidsgreen,50,50);
+            image(lpkidsred,50,50);
+            image(lpkidsblue,50,50);
         }
     } 
     else {
         blendMode(NORMAL);
-        image(lpkidsgray,0,0);
+        image(lpkidsgray,50,50);
+        image(television,0,0);
     }
-    
+     
 }
